1214d87317cb3ff5c980fea947e30bba23d9c548
Keks <keks@htmlacademy.ru>
2025-11-28 23:38:13 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,9 +1,8 @@
 import {generatePhotosDescription} from './data/photosGenerator.js';
-import {getRandomInteger, createRandomIdFromRangeGenerator} from './utils/randomNums.js';
+import {createRandomIdFromRangeGenerator} from './utils/randomNums.js';
 
 
 const commentsIdGenerator = createRandomIdFromRangeGenerator(1, 1000);
 const photos = generatePhotosDescription(25, commentsIdGenerator);
-console.log(photos);
 
 
