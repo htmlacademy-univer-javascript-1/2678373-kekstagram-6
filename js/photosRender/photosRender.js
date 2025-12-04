e868e4b7fc656b9fd67edfdc3b1693a7e95a7c44
Emil Ibragimov <stormelit905@gmail.com>
2025-12-05 06:38:26 +0700

    add alt desc

--- js/photosRender/photosRender.js
+++ js/photosRender/photosRender.js
@@ -7,9 +7,14 @@ function renderPhotosTemplate(data) {
 
   photosData.forEach((element) => {
     const pictureElement = pictureTemplate.content.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = element.url;
+
+    const pictureImg = pictureElement.querySelector('.picture__img');
+    pictureImg.src = element.url;
+    pictureImg.alt = element.description;
+
     pictureElement.querySelector('.picture__likes').textContent = element.likes;
     pictureElement.querySelector('.picture__comments').textContent = element.comments.length;
+
     fragment.appendChild(pictureElement);
   });
 
