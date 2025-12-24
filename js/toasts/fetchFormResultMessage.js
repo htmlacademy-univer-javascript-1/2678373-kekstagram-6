92b686dd8eeacdc832ec602b9a0d5bd87bf903f9
Emil Ibragimov <stormelit905@gmail.com>
2025-12-24 17:02:49 +0700

    remove unnecassary variable

--- js/toasts/fetchFormResultMessage.js
+++ js/toasts/fetchFormResultMessage.js
@@ -2,23 +2,22 @@ import { onFormEscapeKeydown } from '../utils/keyboardControl.js';
 function showMessage(success) {
   const successClass = success ? 'success' : 'error';
   const template = document.querySelector(`#${successClass}`);
-  const page = document;
   if (!success) {
-    page.removeEventListener('keydown', onFormEscapeKeydown);
+    document.removeEventListener('keydown', onFormEscapeKeydown);
   }
   const statusElement = template.content.querySelector(`.${successClass}`).cloneNode(true);
   statusElement.style.zIndex = 1000;
-  page.body.appendChild(statusElement);
+  document.body.appendChild(statusElement);
 
   const inner = statusElement.querySelector(`.${successClass}__inner`);
   const button = statusElement.querySelector(`.${successClass}__button`);
 
   function closeError() {
     statusElement.remove();
-    page.removeEventListener('keydown', onEscKeydown);
-    page.removeEventListener('click', onOutsideClick);
+    document.removeEventListener('keydown', onEscKeydown);
+    document.removeEventListener('click', onOutsideClick);
     if (!success) {
-      page.addEventListener('keydown', onFormEscapeKeydown);
+      document.addEventListener('keydown', onFormEscapeKeydown);
     }
   }
 
@@ -36,8 +35,8 @@ function showMessage(success) {
   }
 
   button.addEventListener('click', closeError);
-  page.addEventListener('keydown', onEscKeydown);
-  page.addEventListener('click', onOutsideClick);
+  document.addEventListener('keydown', onEscKeydown);
+  document.addEventListener('click', onOutsideClick);
 }
 
 export { showMessage };
